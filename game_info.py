9472eba8b5c54a9c38cdcb44fe28bc55247023e4
Abi1122 <79123367+Abi1122@users.noreply.github.com>
2021-09-06 11:42:23 +1200

    Add files via upload

--- game_info.py
+++ game_info.py
@@ -1,7 +1,8 @@
 class player_in:
     def __init__(self,pn,yl):
+        
         self.player_name = pn
         self.year_level = int(yl)
         print(self.year_level)
         print(self.player_name)
-
+        self.subch == nb
